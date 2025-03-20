dd9dbc91c1907b5ed184b48c837d25f1bbe68d69
Sina <sinabanifazl81@gmail.com>
2025-03-20 21:48:25 +0330

    Step 3 - video 5 did watched

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
-from flask import Flask
+from flask import Flask , jsonify , request
 app = Flask(__name__)
 
 
-@app.route('/')
-def main_page():
+@app.route('/v1/process' , methods=["POST"])
+def process():
+    """this is call back from kavenegar. will get sender and message and will check if it is valid , then answers back"""
 
-    '''this is main page of the site'''
-
-    return "hello"
-
-
-@app.route('v1/getsms')
-def get_sms():
-    pass
-
+    data = request.form
+    sender = data[}from]
+    message = data["message"]
+    print(f"received (message) from (sender)")
+    ret = {"message" : "processed"}
+    return jsonify(ret) , 200
 def send_sms():
     pass
 
 def check_serial():
     pass
+
+if __name__ == "__main__":
+    app.run("0.0.0.0" , 5000 , debug=True)
+
